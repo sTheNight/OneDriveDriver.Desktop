41b64846447638a93cf76616258123bbd9658d1e
sTheNight <sthenight@outlook.com>
2026-05-25 11:28:57 +0800

    -

--- Controls/IconButton.axaml.cs
+++ Controls/IconButton.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace OneDriveDriver.Desktop.Controls;
 
@@ -7,8 +8,16 @@ public class IconButton : Button {
     public static readonly StyledProperty<string?> IconProperty =
         AvaloniaProperty.Register<IconButton, string?>(nameof(Icon));
 
+    public static readonly StyledProperty<Color> IconColorProperty =
+        AvaloniaProperty.Register<IconButton, Color>(nameof(IconColor), Colors.Black);
+
     public string? Icon {
         get => GetValue(IconProperty);
         set => SetValue(IconProperty, value);
+    }
+
+    public Color IconColor {
+        get => GetValue(IconColorProperty);
+        set => SetValue(IconColorProperty, value);
     }
 }
