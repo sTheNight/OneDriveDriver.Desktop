450acd7edd0ae999294cd7f2a704d2a9115f5bc0
sTheNight <sthenight@outlook.com>
2026-06-03 09:26:21 +0800

    -

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Rendering.Composition;
@@ -10,12 +11,12 @@ using System.Threading.Tasks;
 namespace OneDriveDriver.Desktop.Views;
 
 public partial class MainWindow : Window {
-    private  MainWindowViewModel _mainWindowViewModel;
-    private const uint _animationDuration = 500;
+    private MainWindowViewModel _mainWindowViewModel = null!;
+    private const uint AnimationDuration = 300;
+
     public MainWindow() {
         InitializeComponent();
         Opened += OnOpened;
-
     }
 
     private void OnOpened(object? sender, EventArgs e) {
@@ -26,16 +27,17 @@ public partial class MainWindow : Window {
     }
 
     private void ViewModelOnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
-        Console.WriteLine(e.PropertyName);
-        if (e.PropertyName == nameof(MainWindowViewModel.IsTaskPanelShow)) {
-            ShowTaskPanelAnimation();
+        switch (e.PropertyName) {
+            case nameof(MainWindowViewModel.IsTaskPanelShow):
+                _ = ShowTaskPanelAnimation();
+                break;
         }
     }
 
-    private async void ShowTaskPanelAnimation() {
+    private async Task ShowTaskPanelAnimation() {
         var compositionVisual = ElementComposition.GetElementVisual(TaskPanel);
         if (compositionVisual is null) return;
-        
+
         var compositor = compositionVisual.Compositor;
         if (_mainWindowViewModel.IsTaskPanelShow) {
             TaskPanel.IsVisible = true;
@@ -45,15 +47,16 @@ public partial class MainWindow : Window {
 
         var hiddenOffset = GetTaskPanelHiddnOffset();
         var shownOffset = GetTaskPanelShownOffset();
-        
+        var easing = new CircularEaseOut();
+
         var slideAnimation = compositor.CreateVector3KeyFrameAnimation();
-        slideAnimation.InsertKeyFrame(0f,_mainWindowViewModel.IsTaskPanelShow ? hiddenOffset : shownOffset);
-        slideAnimation.InsertKeyFrame(1f,_mainWindowViewModel.IsTaskPanelShow ? shownOffset : hiddenOffset);
-        slideAnimation.Duration = TimeSpan.FromMilliseconds(_animationDuration);
-        
+        slideAnimation.InsertKeyFrame(0f, _mainWindowViewModel.IsTaskPanelShow ? hiddenOffset : shownOffset, easing);
+        slideAnimation.InsertKeyFrame(1f, _mainWindowViewModel.IsTaskPanelShow ? shownOffset : hiddenOffset, easing);
+        slideAnimation.Duration = TimeSpan.FromMilliseconds(AnimationDuration);
+
         compositionVisual.StartAnimation("Offset", slideAnimation);
 
-        await Task.Delay(TimeSpan.FromMilliseconds(_animationDuration));
+        await Task.Delay(TimeSpan.FromMilliseconds(AnimationDuration));
 
         if (!_mainWindowViewModel.IsTaskPanelShow) {
             TaskPanel.IsVisible = false;
@@ -64,6 +67,7 @@ public partial class MainWindow : Window {
     private Vector3 GetTaskPanelHiddnOffset() {
         return new Vector3((float)-TaskPanel.Width, 0, 0);
     }
+
     private Vector3 GetTaskPanelShownOffset() {
         return new Vector3(0, 0, 0);
     }
