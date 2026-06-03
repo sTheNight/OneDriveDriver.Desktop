3698f654b3c834ab13111ad6ca8d7a9b305e8f57
sTheNight <sthenight@outlook.com>
2026-06-03 10:15:46 +0800

    -

---
+++ Theme/AppThemeVariants.cs
@@ -0,0 +1,7 @@
+using Avalonia.Styling;
+
+namespace OneDriveDriver.Desktop.Theme;
+
+public static class AppThemeVariants {
+    public static readonly ThemeVariant LightBlue = new("LightBlue", ThemeVariant.Light);
+}
