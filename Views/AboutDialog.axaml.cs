750d5d1439fcd57fb0848c05203f19bdabc80836
sTheNight <sthenight@outlook.com>
2026-06-04 23:42:01 +0800

    refactor: 拆分 AboutDialog 与 BottomSheet

---
+++ Views/AboutDialog.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Controls;
+
+namespace OneDriveDriver.Desktop.Views;
+
+public partial class AboutDialog : UserControl {
+    public AboutDialog() {
+        InitializeComponent();
+    }
+}
---
+++ Views/BottomSheet.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OneDriveDriver.Desktop.Views;
+
+public partial class BottomSheet : UserControl {
+    public BottomSheet() {
+        InitializeComponent();
+    }
+}
